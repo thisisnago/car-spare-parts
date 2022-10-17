dad061a00f1494b7302b936cb36764b57a53aeee
thisisnago <yurii.nagorniak@gmail.com>
2022-10-17 21:31:07 +0000

    Deleted items

--- site/Car Parts Seller Website Template Demo_files/in.php
+++
@@ -1,7 +0,0 @@
-// (☞ﾟ∀ﾟ)☞ static32
-if( window._cgen ) {
-_cgen.set_cookie('_jsuid', '2836893815');
-var unpoco_101207210=1;
-_cgen.set_cookie('_no_tracky_101207210', 1, 3600, 1 );
-}
-// bad site id (╯°□°）╯︵ ┻━┻
